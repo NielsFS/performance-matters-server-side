3d242cc97112969529313a08e9afed5932664e89
Niels <nielsschopman@gmail.com>
2019-05-20 16:43:57 +0200

    Service worker added caching

--- Herkansing/public/sw.js
+++ Herkansing/public/sw.js
@@ -1,4 +1,29 @@
+const cache = 'v1'
+const cacheAssets = [
+	// 'index.html'
+	'views/layouts/main.handlebars',
+	'views/partials/header.handlebars',
+	// 'views/partials/loadingSpinner.handlebars',
+	'views/detail.handlebars',
+	'views/index.handlebars',
+	'public/css/style-min.css',
+	'public/javascript/bundle.js'
+]
 
 self.addEventListener('install', (event) => {
 	console.log('service worker installed')
+
+	event.waitUntil(
+		caches
+		.open(cache)
+		.then(cache => {
+			console.log('caching files')
+			cache.addAll(cacheAssets)
+		}) 
+		.then(() => self.skipWaiting())
+	)
+})
+
+self.addEventListener('activate', (event) => {
+	console.log('serviceWorker activated')
 })
